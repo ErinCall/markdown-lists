f67ede5f6f034df5e87934d24f750cba9aa7966a
Andrew Lorente <hello@andrewlorente.com>
2014-12-14 11:51:37 -0800

    Simple app that shows the incoherent rendering

--- src/Site.hs
+++ src/Site.hs
@@ -9,63 +9,37 @@ module Site
   ) where
 
 ------------------------------------------------------------------------------
-import           Control.Applicative
-import           Data.ByteString (ByteString)
-import qualified Data.Text as T
+import           Data.ByteString             (ByteString)
+import           Data.Default                (def)
+import           Data.Monoid                 (mempty)
+import qualified Data.Text.Lazy              as L
 import           Snap.Core
 import           Snap.Snaplet
-import           Snap.Snaplet.Auth
-import           Snap.Snaplet.Auth.Backends.JsonFile
 import           Snap.Snaplet.Heist
-import           Snap.Snaplet.Session.Backends.CookieSession
 import           Snap.Util.FileServe
+import qualified Text.Blaze.Renderer.XmlHtml as X (renderHtml)
+import           Text.Markdown               (markdown, MarkdownSettings(..))
+import qualified Text.XmlHtml                as X
 import           Heist
-import qualified Heist.Interpreted as I
+import qualified Heist.Interpreted           as I
 ------------------------------------------------------------------------------
 import           Application
 
-
-------------------------------------------------------------------------------
--- | Render login form
-handleLogin :: Maybe T.Text -> Handler App (AuthManager App) ()
-handleLogin authError = heistLocal (I.bindSplices errs) $ render "login"
+markdownSplice :: Monad n => I.Splice n
+markdownSplice = I.runNodeList $ X.docContent $ X.renderHtml $ markdown settings $ L.concat
+                [ "* first item"
+                , "* second item"
+                , "* third item"
+                ]
   where
-    errs = maybe noSplices splice authError
-    splice err = "loginError" ## I.textSplice err
+    settings = def { msXssProtect = False }
 
+index :: Handler App App ()
+index = method GET $ render "index"
 
-------------------------------------------------------------------------------
--- | Handle login submit
-handleLoginSubmit :: Handler App (AuthManager App) ()
-handleLoginSubmit =
-    loginUser "login" "password" Nothing
-              (\_ -> handleLogin err) (redirect "/")
-  where
-    err = Just "Unknown user or password"
-
-
-------------------------------------------------------------------------------
--- | Logs out and redirects the user to the site index.
-handleLogout :: Handler App (AuthManager App) ()
-handleLogout = logout >> redirect "/"
-
-
-------------------------------------------------------------------------------
--- | Handle new user form submit
-handleNewUser :: Handler App (AuthManager App) ()
-handleNewUser = method GET handleForm <|> method POST handleFormSubmit
-  where
-    handleForm = render "new_user"
-    handleFormSubmit = registerUser "login" "password" >> redirect "/"
-
-
-------------------------------------------------------------------------------
--- | The application's routes.
 routes :: [(ByteString, Handler App App ())]
-routes = [ ("/login",    with auth handleLoginSubmit)
-         , ("/logout",   with auth handleLogout)
-         , ("/new_user", with auth handleNewUser)
-         , ("",          serveDirectory "static")
+routes = [ ("/",    ifTop index)
+         , ("",     serveDirectory "static")
          ]
 
 
@@ -74,15 +48,11 @@ routes = [ ("/login",    with auth handleLoginSubmit)
 app :: SnapletInit App App
 app = makeSnaplet "app" "An snaplet example application." Nothing $ do
     h <- nestSnaplet "" heist $ heistInit "templates"
-    s <- nestSnaplet "sess" sess $
-           initCookieSessionManager "site_key.txt" "sess" (Just 3600)
 
-    -- NOTE: We're using initJsonFileAuthManager here because it's easy and
-    -- doesn't require any kind of database server to run.  In practice,
-    -- you'll probably want to change this to a more robust auth backend.
-    a <- nestSnaplet "auth" auth $
-           initJsonFileAuthManager defAuthSettings sess "users.json"
+    let config = mempty {
+        hcInterpretedSplices = "someRenderedMarkdown" ## markdownSplice
+      }
     addRoutes routes
-    addAuthSplices h auth
-    return $ App h s a
+    addConfig h config
+    return $ App h
 
